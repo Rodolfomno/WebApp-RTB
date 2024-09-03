daf22c97943e98aff194abcefaf324d400aafa16
rodolfomno <rodolfo_mno@hotmail.com>
2024-09-02 21:41:09 -0300

    feat: create filters functio

--- frontend/src/pages/homePage.tsx
+++ frontend/src/pages/homePage.tsx
@@ -1,24 +1,29 @@
-import { Table } from "../components/table/dataTable";
+import { DataType, Table } from "../components/table/dataTable";
 import { useEffect, useState } from "react"
-import { HomeContainer } from "./style";
+import { ButtonsFilter, ClearButton, FilterButton, HomeContainer } from "./style";
 import { Filter } from "../components/filters/filter";
 import { FilterRow, FiltersContainer } from "../components/filters/style";
+import { OrdersRanking } from "../components/ordersRanking/ordersRanking";
+import Header from "../components/header/header";
 
 
 export function HomePage () {
-    const [data, setData] = useState([])
+    const [data, setData] = useState<DataType[]>([])
+    const [filteredData, setFilteredData] = useState<DataType[]>([])
     const [topSellers, setTopSellers] = useState();
     const [countriesOptions, setCountriesOptions] = useState();
     const [sellersOptions, setSellerOptions] = useState();
-    // const [filteredCountries, setFilteredCountries] = useState();
-    // const [filteredSellers, setFilteredSellers] = useState();
-
+    const [filteredCountries, setFilteredCountries] = useState<{ value: string, label: string }[]>();
+    const [filteredSellers, setFilteredSellers] = useState<{ value: string, label: string }[]>();
 
     useEffect(() => {
 
       fetch('http://localhost:3001/get-all-orders')
         .then(response => response.json())
-        .then((json) => setData(json))
+        .then((json) => {
+          setData(json);
+          setFilteredData(json);
+        })
         .catch(error => console.error(error));
 
       fetch('http://localhost:3001/get-filters-options')
@@ -37,22 +42,56 @@ export function HomePage () {
 
     }, []);
 
-    console.log(topSellers);
+  
+    function handleFilter () {
+      let hasFiltered = false;
+      if (filteredCountries && data) {
+        const filtered = data.filter(item => filteredCountries.some((option) => option.label === item.country));
+
+        setFilteredData(filtered)
+        hasFiltered = true
+      }
+      if (filteredSellers && data) {
+        const filtered = data.filter(item => filteredSellers.some((option) => option.label === item.sellerName));
+        if (hasFiltered) {
+          const alreadyFiltered = filteredData.filter(item => filteredSellers.some((option) => option.label === item.sellerName));
+          setFilteredData(alreadyFiltered)
+        } else {
+          setFilteredData(filtered)
+        }
+      }
+
+    }
+
+    function clearFilters () {
+      setFilteredData(data);
+    }
+
+    
+
     return (
-        <HomeContainer> 
-        <h1 style={{ color: 'grey' }}>
-            Orders Table
-        </h1>
-        <FilterRow>
-            <FiltersContainer>
-                <div style={{ textAlign: 'center', lineHeight: '60px' }}>
-                    Filters
-                </div>
-                { countriesOptions &&  <Filter title="Countries" options={countriesOptions} />}
-                { sellersOptions &&      <Filter title="Sellers" options={sellersOptions} />}
-            </FiltersContainer>
-        </FilterRow>
-        { data && <Table data={data} /> }
-        </HomeContainer>
+        <>
+          <Header />
+            <HomeContainer> 
+              { topSellers && <OrdersRanking data={topSellers} />}
+            <FilterRow>
+                <FiltersContainer>
+
+                    <ButtonsFilter>
+                        <FilterButton onClick={() => handleFilter()}>
+                          Apply
+                        </FilterButton>
+                        <ClearButton onClick={() => clearFilters()}>
+                          Cancel
+                        </ClearButton>
+                    </ButtonsFilter>
+                    { countriesOptions && <Filter title="Countries" options={countriesOptions} setFilter={setFilteredCountries} />}
+                    { sellersOptions && <Filter title="Sellers" options={sellersOptions} setFilter={setFilteredSellers} />}
+                </FiltersContainer>
+            </FilterRow>
+            <Table  data={filteredData}/>
+
+          </HomeContainer>
+        </>
     );
 }
--- frontend/src/pages/style.tsx
+++ frontend/src/pages/style.tsx
@@ -7,4 +7,33 @@ flex-direction: column;
 padding-left: 60px;
 padding-right: 60px;
 align-items: center;
+padding-bottom: 100px;
+`
+
+export const FilterButton = styled.div`
+width: 50px;
+height: 30px;
+background-color: blue;
+font-size: 13px;
+color: #f0f0f0;
+padding: 8px;
+border-radius: 14px;
+    &: hover {
+        cursor: pointer;
+    }
+`
+
+export const ButtonsFilter = styled.div`
+padding-top: 25px;
+`
+
+export const ClearButton = styled.div`
+padding-top: 3px;
+text-decoration: underline;
+font-size: 12px;
+color: red;
+text-align: center;
+&: hover {
+    cursor: pointer;
+}
 `
