47ff4ec071d3b9c20371e4362c24de8df37f18d4
rodolfomno <rodolfo_mno@hotmail.com>
2024-09-02 21:41:51 -0300

    feat: create navigate buttons

--- frontend/src/components/button/button.tsx
+++ frontend/src/components/button/button.tsx
@@ -1,9 +1,22 @@
+import { useLocation, useNavigate } from "react-router-dom";
 import { ButtonComponent } from "./styles";
 
 export default function Button ({ title }: { title: string}) {
+    const path = useLocation().pathname
+    const navigate = useNavigate()
+    function onButtonClick () {
+
+        if (path === '/devInfo' && title === 'Orders') {
+            navigate('/')
+        }
+        if (path === '/' && title === 'Info') {
+            navigate('/devInfo')
+        }
+    }
+
     return (
         <ButtonComponent
-        onClick={() => console.log('SALVE')}>
+        onClick={onButtonClick}>
             <p style={{ color: 'white' }}>
               { title }
             </p>
---
+++ frontend/src/components/cardRanking/card.tsx
@@ -0,0 +1,21 @@
+import { formatToCurrency } from "../../helpers/helpers";
+import { CardContainer, Title, SellerContainer } from "./styles";
+
+
+export function Card ({ data, place }: { data: { id: number, name: string, totalSales: number }[], place: number }) {
+    return(
+        <CardContainer>
+            <Title>
+                {  `${place}º place ` }
+            </Title>
+            <SellerContainer>
+                <div style={{ color: '#383838', fontWeight: 'bold' }}>
+                    { data[place -1].name}
+                </div>
+                <div style={{ textAlign: 'end', color: '#383838', fontWeight: 'bold' }}>
+                    { `Total: ${formatToCurrency(data[place -1].totalSales)}` }
+                </div>
+            </SellerContainer>
+        </CardContainer>
+    )
+}
